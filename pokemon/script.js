5c56d6f0a3030c05851c628464cfeb7796f53d1a
Atishay Jain <atishay2007@gmail.com>
2026-06-26 12:32:57 +0530

    Added pokedex

--- pokemon/script.js
+++ pokemon/script.js
@@ -8,9 +8,25 @@ const type = document.querySelector("#type");
 const abilities = document.querySelector("#abilities");
 const pokemonImage = document.querySelector("#pokemonImage");
 
-searchBtn.addEventListener("click", ()=>searchPokemon(searchBar.value));
-searchBar.addEventListener("enter", ()=>searchPokemon(searchBar.value));
 
-function searchPokemon(pokemonName){
+searchBtn.addEventListener("click", () => searchPokemon(searchBar.value));
+searchBar.addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+        searchPokemon(searchBar.value);
+    }
+});
+async function searchPokemon(pokemonName) {
+
+    pokemonName = pokemonName.toLowerCase();
     console.log(pokemonName);
+    const url = `https://pokeapi.co/api/v2/pokemon/${pokemonName}`;
+    const response = await fetch(url);
+    const data = await response.json();
+
+    name.textContent = data.name;
+    height.textContent = data.height;
+    weight.textContent = data.weight;
+
+    console.log(response);
+
 }
